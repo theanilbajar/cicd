a607ebb33df9a6d936726538693ad5017a766c4e
Anil <anil946233@gmail.com>
2020-01-23 00:44:35 +0530

    Update wget command for heroku toolbelt

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget -qo https://toolbelt.heroku.com/install-ubuntu.sh | sh
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku container:login
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
